9fdbd732da10ff2100a25697bc031af5a3e8f425
MosesLevin <moses.levin14@gmail.com>
2025-01-30 15:38:26 +0100

    fixed flickering by removing css transitions on motion div, added onanimationcomplete effects

--- src/app/sections/Services.tsx
+++ src/app/sections/Services.tsx
@@ -58,7 +58,8 @@ const services: interfaceServices[] = [
 
 const Services = () => {
   const [isAnimationComplete, setIsAnimationComplete] = useState(false)
-  const handleAnimationEnd = () => {
+
+  const handleAnimationComplete = () => {
     console.log('Animation ended')
     setIsAnimationComplete(true)
   }
@@ -77,22 +78,24 @@ const Services = () => {
           {services.map((service, index) => (
             <motion.div
               key={index}
-              className="relative md:rounded-lg p-4 group text-center transition-all duration-200 md:hover:transform md:hover:translate-y-[-5px] md:shadow-lg "
-              initial={{ opacity: 0, transform: 'translateX(100px)' }}
-              whileInView={{ opacity: 1, transform: 'translateX(0px)' }}
+              className="relative md:rounded-lg p-4 group text-center md:shadow-lg"
+              initial={{ opacity: 0, x: 100 }} // Using 'x' instead of 'transform'
+              whileInView={{ opacity: 1, x: 0 }}
               transition={{ type: 'spring', delay: 0.1 }}
-              viewport={{ amount: 'all', once: true, margin: '100px' }}
-              onAnimationEnd={handleAnimationEnd}
+              viewport={{ amount: 'all', once: false, margin: '100px' }}
+              onAnimationComplete={handleAnimationComplete}
             >
               {/* Content */}
-              <div className="md:flex items-center md:justify-between justify-center">
+              <div className="md:flex items-center md:justify-between justify-center w-1/2  md:w-full translate-x-1/2 md:translate-x-0 ">
                 <div className="bg-gray-200 size-12 rounded-full flex justify-center items-center text-white mx-auto md:mx-0 mb-6 md:mb-0">
                   {service.icon}
                 </div>
                 <div>
                   <h3
                     className={`text-xl font-semibold text-black md:inline-flex my-4 mt-8 ${
-                      isAnimationComplete ? 'underline-with-svg' : ''
+                      isAnimationComplete
+                        ? 'shadow-md md:shadow-none text-white md:text-black bg-p1c2 rounded-full button-animation transition-all duration-300 md:bg-transparent '
+                        : ''
                     }`}
                   >
                     {service.title}
