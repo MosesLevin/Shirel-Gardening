f508cb1936e16c5075b1224e1143d909532bcf14
MosesLevin <moses.levin14@gmail.com>
2025-02-05 16:01:54 +0100

    changed timeline placement for text to be aligned with rest of sections, sectionheader mx reduced for more text place on mobile

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -41,13 +41,13 @@ function About() {
           eyeCatch="מי אנחנו"
           underlinedWord="גינון בר-קיימא"
         />
-        <p className="mt-8 max-w-3xl text-gray-900 text-lg text-end p-1 mx-10">
+        <p className="mt-8 max-w-3xl text-gray-900 text-lg text-end p-1 mx-4">
           לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית גולר מונפרר סוברט
           לורם שבצק יהול, לכנוץ בעריר גק ליץ, ושבעגט ליבם סולגק. בראיט ולחת צורק
           מונחף, בגורמי מגמש. תרבנך וסתעד לכנו סתשם השמה - לתכי מורגם בורק? לתיג
           ישבעס.
         </p>
-        <p className="mt-4 max-w-3xl text-gray-900 text-lg text-end p-1 mx-10">
+        <p className="mt-4 max-w-3xl text-gray-900 text-lg text-end p-1 mx-4">
           וסטיבולום אט דולור, קראס אגת לקטוס וואל אאוגו וסטיבולום סוליסי טידום
           בעליק.
         </p>
